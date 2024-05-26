f68c69cb153c9b172c4369c7d93320a6957774c7
lkhorasandzhian <levon.kh228@gmail.com>
2024-05-26 21:10:40 +0300

    Add initial controller for users

---
+++ AtonSolution/AtonWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AtonWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtonWebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly StorageContext _context;
+
+		public UsersController(StorageContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+		{
+			return await _context.Users.ToListAsync();
+		}
+	}
+}
